a1b8e18747350443e116f1bc09e7f52ac8d8e5b6
Kanyaton Somjit <113116925+Kanyatonsita@users.noreply.github.com>
2024-02-26 14:24:53 +0100

    SettUpView

--- app/src/main/java/com/example/cookbookandshoplist/MainActivity.kt
+++ app/src/main/java/com/example/cookbookandshoplist/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.cookbookandshoplist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun goToLogInActivity(view: View) {
+        val intent = Intent(this, LogInActivity::class.java)
+        startActivity(intent)
     }
 }
